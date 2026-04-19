da1956c9d0c18b9546cf8e34144abc34c1112107
Ayushman Sahoo <sahooayushman002@gmail.com>
2026-04-19 23:29:36 +0530

    Add files via upload

---
+++ Q15.c
@@ -0,0 +1,47 @@
+/*
+Problem: Given a matrix, calculate the sum of its primary diagonal elements. The primary diagonal consists of elements where row index equals column index.
+
+Input:
+- First line: two integers m and n
+- Next m lines: n integers each
+
+Output:
+- Print the sum of the primary diagonal elements
+
+Example:
+Input:
+3 3
+1 2 3
+4 5 6
+7 8 9
+
+Output:
+15
+
+Explanation:
+1 + 5 + 9 = 15
+*/
+
+#include <stdio.h>
+int main()
+{
+    int matrix[100][100], m, n, sum = 0;
+    printf("Enter the number of rows : ");
+    scanf("%d", &m);
+    printf("Enter the number of columns : ");
+    scanf("%d", &n);
+    printf("Enter the elements of the matrix : \n");
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (int i = 0; i < m && i < n; i++) 
+    {
+        sum += matrix[i][i];
+    }
+    printf("Sum of primary diagonal elements : %d\n", sum);
+    return 0;
+}
